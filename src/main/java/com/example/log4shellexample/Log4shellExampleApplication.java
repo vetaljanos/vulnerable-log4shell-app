7feee267c3ccf55e4be632a874275c5dda0cbd23
rvs <nologinb@gmail.com>
2023-06-08 16:52:34 +0300

    initial

---
+++ src/main/java/com/example/log4shellexample/Log4shellExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.log4shellexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4shellExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4shellExampleApplication.class, args);
+    }
+
+}
